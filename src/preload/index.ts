cb557c229ee7903477f401270e46d06008d3a334
Rubrik25 <vitaliystanchenko@gmail.com>
2025-02-26 21:51:57 +0200

    Dotenv test. (Preload > Renderer)

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,9 +1,10 @@
 import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
+import * as dotenv from 'dotenv'
 
 // Custom APIs for renderer
 const api = {}
-
+dotenv.config()
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
@@ -11,6 +12,9 @@ if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
     contextBridge.exposeInMainWorld('api', api)
+    contextBridge.exposeInMainWorld('env', {
+      USER_ID: process.env.USER_ID
+    })
   } catch (error) {
     console.error(error)
   }
--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
 
+const userId = ref<string | null>(null)
+
+onMounted(() => {
+  userId.value = window.env?.USER_ID || 'No user ID found'
+})
+</script>
 <template>
   <strong>Current route path:</strong> {{ $route.fullPath }}
+  <br />
+  <strong>Current user id:</strong> {{ userId }}
   <nav>
     <RouterLink to="/">Go to home</RouterLink>
     <br />
