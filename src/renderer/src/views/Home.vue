0f1c43734baaaa9db2c61181befa374246bb7f9c
Rubrik25 <vitaliystanchenko@gmail.com>
2025-02-28 21:44:25 +0200

    Add blur and minor things.

--- src/renderer/src/views/Home.vue
+++ src/renderer/src/views/Home.vue
@@ -29,6 +29,7 @@ const searchObjects = async (): Promise<void> => {
     />
   </div>
   <div class="objects m-4">
+    <p class="text-2xl text-center">Anime</p>
     <div class="grid grid-cols-3 gap-4 sm:grid-cols-3 xs:grid-cols-1 max-w-4xl mx-auto">
       <div
         v-for="anime in animeList.data"
@@ -56,14 +57,19 @@ const searchObjects = async (): Promise<void> => {
         </div>
       </div>
     </div>
-
+    <p class="text-2xl text-center">Manga</p>
     <div class="grid grid-cols-3 gap-4 sm:grid-cols-3 xs:grid-cols-1 mt-8 max-w-4xl mx-auto">
       <div
         v-for="manga in mangaList.data"
         :key="manga.id"
         class="manga-item bg-white rounded-lg shadow-md overflow-hidden flex flex-col h-96"
       >
-        <img :src="manga.cover.default" :alt="manga.rus_name" class="w-full h-48 object-cover" />
+        <img
+          :src="manga.cover.default"
+          :alt="manga.rus_name"
+          class="w-full h-48 object-cover"
+          :class="{ blur: manga.site === 2 || manga.site === 4 }"
+        />
         <div class="content p-4 flex flex-col justify-between flex-grow">
           <h2 class="text-xl text-gray-800 mb-2">{{ manga.rus_name }}</h2>
           <p class="text-gray-500 text-sm">{{ manga.type.label }}</p>
